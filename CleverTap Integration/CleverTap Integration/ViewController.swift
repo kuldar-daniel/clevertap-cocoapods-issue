44ac1427f16a43611a48a6f2d36d1845e5085318
Kuldar-Daniel Kokorev <kuldar-daniel.kokorev@mooncascade.com>
2023-06-28 10:29:37 +0300

    Initial commit

---
+++ CleverTap Integration/CleverTap Integration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleverTap Integration
+//
+//  Created by kuldar-daniel on 28.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
